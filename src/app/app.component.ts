54abdb939092b62285c3b7d9eb2b3ba420e4d2c3
Belma <belma.muratovic99@hotmail.com>
2025-03-22 14:05:07 +0100

    Responsive layout added & mobile and desktop responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Library';
+  isHandset: boolean = false;
+  isTablet: boolean = false;
+  isWeb: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .pipe(
+        map((result) => result.matches),
+        map((isHandset) => {
+          if (isHandset) {
+            console.log('Screen size is Mobile');
+          }
+          this.isHandset = isHandset;
+          return isHandset;
+        })
+      )
+      .subscribe();
+
+    this.breakpointObserver
+      .observe([Breakpoints.Tablet])
+      .pipe(
+        map((result) => result.matches),
+        map((isTablet) => {
+          if (isTablet) {
+            console.log('Screen size is Tablet');
+          }
+          this.isTablet = isTablet;
+          return isTablet;
+        })
+      )
+      .subscribe();
+
+    this.breakpointObserver
+      .observe([Breakpoints.Web])
+      .pipe(
+        map((result) => result.matches),
+        map((isWeb) => {
+          if (isWeb) {
+            console.log('Screen size is Web (Desktop)');
+          }
+          this.isWeb = isWeb;
+          return isWeb;
+        })
+      )
+      .subscribe();
+  }
 }
